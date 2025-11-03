330e113fc2bc28c6a0be813bca006e5d41d570bc
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-11-04 00:01:55 +0530

    Implemt ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-	res.status(200).send('Hello from Acquisitions!')
-})
+  res.status(200).send('Hello from Acquisitions!');
+});
 
 export default app;
